f6874e7cb4bff9bcb8dbfeb80675ef017d9d0003
VeronicaCian <60195625+VeronicaCian@users.noreply.github.com>
2020-03-13 14:27:29 +0100

    Creacion subMenu Producto

--- GestionAlmacen/src/Main.java
+++ GestionAlmacen/src/Main.java
@@ -57,5 +57,49 @@ public class Main {
         return c;
 
     }
+	public static Producto subMenuProducto() throws NumberFormatException, IOException {
+        java.io.BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        //Inicializacion de datos de producto
+        int codigo = 0;
+        String nombre = null;
+        String descripcion = null;
+        int stock = 0;
+        char pasillo = ' ';
+        int estanteria = 0;
+        int estante = 0;
+        boolean estado = false;
+        String estado_aux = null;
+        System.out.println("Introduce datos del producto:");
+        //Introducción de datos por el usuario
+        System.out.println("Introduce codigo de 10 números:");
+        codigo = Integer.parseInt(in.readLine());;
+        System.out.println("Introduce nombre:");
+        nombre = in.readLine();
+        System.out.println("Introduce descripcion:");
+        descripcion = in.readLine();
+        System.out.println("Introduce stock:");
+        stock = Integer.parseInt(in.readLine());;
+        System.out.println("Introduce la localización del producto en el almacen:");
+        System.out.println("Pasillo (A-Z):");
+        pasillo = (in.readLine()).charAt(0);
+        System.out.println("Estanteria (0-10):");
+        estanteria = Integer.parseInt(in.readLine());;
+        System.out.println("Introduce estante (0-5):");
+        estante = Integer.parseInt(in.readLine());;
+        System.out.println("---Fin de localización---");
+        System.out.println("Introduce si el producto está en almacen (y/n):");
+        estado_aux = in.readLine();
+        if (estado_aux == "y"  estado_aux == "yes"  estado_aux == "si") {
+            estado = true;
+        }else if(estado_aux == "n" || estado_aux== "no"){
+            estado = false;
+        }
+        System.out.println("---FIN DE PRODUCTO---");
+
+        Producto p = new Producto(codigo, nombre, descripcion, stock, pasillo,
+                estanteria, estante, estado);
+        return p;
+    }
 
 }
